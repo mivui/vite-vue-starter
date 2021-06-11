4688732152bc57d6e1a7e57cbd64521476dc62d6
LvXiaoBu <lv_xiao_bu@qq.com>
2021-06-11 21:19:42 +0800

    Update Dependencies

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -4,15 +4,8 @@ import styles from './style/index.module.less';
 export default defineComponent({
   name: 'Index',
   setup() {
-    /* Component Data */
     document.title = import.meta.env.VITE_APP_TITLE;
-    /* Component Methods */
 
-    /* Component Mounted */
-
-    /* Component Watch */
-
-    /* Component Render */
     return () => {
       return <div class={styles.helloWorld}>Hello World!</div>;
     };
