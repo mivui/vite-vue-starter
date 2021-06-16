3ae01cd4759d37721e028504bb014b578e8ba716
LvXiaoBu <lv_xiao_bu@qq.com>
2021-06-16 21:36:19 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -26,21 +26,20 @@ export default defineConfig({
     //   ],
     // }),
   ],
-  // server: {
-  //   host: '0.0.0.0',
-  //   port: 80,
-  //   https: false,
-  //   open: false,
-  //   cors: true,
-  //   proxy: {
-  //     // 选项写法
-  //     '/api': {
-  //       target: 'http://127.0.0.1:8080',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    https: false,
+    open: false,
+    cors: true,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://127.0.0.1:8080',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //   },
+    // },
+  },
   css: {
     preprocessorOptions: {
       less: {
