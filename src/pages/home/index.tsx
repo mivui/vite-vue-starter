4d873353ba20d90e8790fe7bd139767302762697
LvXiaoBu <lv_xiao_bu@qq.com>
2021-06-17 17:31:11 +0800

    update vite config

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { defineComponent } from 'vue';
+import Logo from '@/assets/logo.png';
 import styles from './style/index.module.less';
 
 export default defineComponent({
@@ -7,7 +8,12 @@ export default defineComponent({
     document.title = import.meta.env.VITE_APP_TITLE;
 
     return () => {
-      return <div class={styles.helloWorld}>Hello World!</div>;
+      return (
+        <div class={styles.helloWorld}>
+          <img src={Logo} alt="Vue" />
+          <div>Hello World!</div>
+        </div>
+      );
     };
   },
 });
