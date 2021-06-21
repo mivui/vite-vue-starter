1080efa8e7375b70c073703abcbc414484b0ffb4
LvXiaoBu <lv_xiao_bu@qq.com>
2021-06-21 17:29:03 +0800

    feat: support vuex

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import router from '@/router';
+import vuex from '@/store';
 import App from './App';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(vuex).mount('#app');
---
+++ src/store/index.ts
@@ -0,0 +1,6 @@
+import { createStore } from 'vuex';
+import { RootState } from './state';
+
+const store = createStore<RootState>({});
+
+export default store;
