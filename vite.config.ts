601c839c07fda7da367c86574cd23a386c5c2fe9
LvXiaoBu <49021590+uinio@users.noreply.github.com>
2021-08-25 20:46:00 +0800

    support: chrome>=49 and firefox>=52

--- vite.config.ts
+++ vite.config.ts
@@ -22,18 +22,7 @@ export default defineConfig({
     }),
     ViteStyleImport({
       libs: [
-        {
-          libraryName: 'element-plus',
-          esModule: true,
-          ensureStyleFile: true,
-          resolveStyle: (name) => {
-            name = name.slice(3);
-            return `element-plus/packages/theme-chalk/src/${name}.scss`;
-          },
-          resolveComponent: (name) => {
-            return `element-plus/lib/${name}`;
-          },
-        },
+        // add ui library
       ],
     }),
   ],
