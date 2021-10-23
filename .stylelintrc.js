29f2713a9bf186c5f89df00e9d5824673e034089
LvXiaoBu <49021590+uinio@users.noreply.github.com>
2021-10-23 09:45:35 +0800

    support stylelint 14

---
+++ .stylelintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    'stylelint-config-html',
+    'stylelint-config-recommended',
+    'stylelint-config-standard-scss',
+  ],
+  overrides: [
+    {
+      files: ['*.vue', '**/*.vue'],
+      rules: {
+        'unit-allowed-list': ['em', 'rem', 's'],
+      },
+    },
+  ],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['deep', 'global'],
+      },
+    ],
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted'],
+      },
+    ],
+  },
+};
