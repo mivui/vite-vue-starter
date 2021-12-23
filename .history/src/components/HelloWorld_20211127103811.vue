f9dae79d8269568cf8dffd09f5ba7b50763bc975
LvXiaoBu <49021590+uinio@users.noreply.github.com>
2021-12-23 17:39:14 +0800

    update vite2.7.6

--- .history/src/components/HelloWorld_20211127103811.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>{{ state.title }}</div>
-</template>
-
-<script lang="ts" setup>
-import { reactive } from 'vue';
-123
-const state = reactive({ title: 'HelloWord' });
-</script>
--- .history/src/components/HelloWorld_20211127103830.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>{{ state.title }}</div>
-</template>
-
-<script lang="ts" setup>
-import { reactive } from 'vue';
-const state = reactive({ title: 'HelloWord' });
-</script>
