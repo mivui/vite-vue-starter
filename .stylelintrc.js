465abe6aef39811c0e5fb9436bcaf1bf4ed2edc6
LvXiaoBu <49021590+uinio@users.noreply.github.com>
2022-03-13 19:14:17 +0800

    update config stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'stylelint-config-prettier',
+    'stylelint-config-recommended',
+    'stylelint-config-standard-scss',
+    'stylelint-config-standard-vue/scss',
+  ],
+};
