6504b362e2bd42231635f708a31286824b33cae5
LvXiaoBu <49021590+uinio@users.noreply.github.com>
2022-03-31 06:45:59 +0800

    update vite2.9

--- .stylelintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [
-    'stylelint-config-prettier',
-    'stylelint-config-recommended',
-    'stylelint-config-standard-scss',
-    'stylelint-config-standard-vue/scss',
-  ],
-};
